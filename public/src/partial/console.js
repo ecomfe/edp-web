650a9b81a331e7874ef5e4ad57e5393ab45874ef
erik <erik168@163.com>
2014-12-25 14:34:26 +0800

    console output hack for build

--- public/src/partial/console.js
+++ public/src/partial/console.js
@@ -17,7 +17,6 @@ define(function (require) {
 
     getPanel().style.display = 'none';
     var isFold = 0;
-
     var texts = [];
 
     var exports = {
@@ -57,6 +56,22 @@ define(function (require) {
         log: function (output) {
             var lines = output.replace(/(^[\x0d\x0a]+|[\x0d\x0a]+$)/g, '').split('\n');
             texts.push.apply(texts, lines);
+
+            // build 过程可能处理很多文件，每个文件都会有一条提示
+            // 在 cli 下，后面的提示会覆盖前面的提示
+            // 但是 child_process 运行下，相关 console 信息缺失
+            // 所以这里专门针对 build 的提示信息特征，做了一个 hack
+            // 以防止输出的提示信息过长，不方便看，也拖慢网页速度
+            var processReg = /^\s+\(\d+ms\)\s+[⋅.]{3,}\s+(\[\d+\/\d+\]:[^\(\)]+\(\d+ms\))?/i;
+            var len = texts.length - 1;
+            while (len--) {
+                var current = texts[len];
+                var next = texts[len + 1];
+                if (processReg.test(next) && (/^\s*$/.test(current) || processReg.test(current))) {
+                    texts.splice(len, 1);
+                }
+            }
+
             getTTY().value = texts.join('\n');
         },
 
