8687789ca5dc92f653b098666b3ed5d9535fec88
erik <erik168@163.com>
2014-12-29 20:38:08 +0800

    robustness

--- public/src/launch.js
+++ public/src/launch.js
@@ -1,35 +1,70 @@
+/**
+ * @file 命令运行模块
+ * @author errorrik[errorrik@gmail.com]
+ */
+
 define(function (require) {
-    function gloOnData() {
-    }
 
-    function gloOnExit() {
-    }
+    var gloOnData;
+    var gloOnExit;
+    var launching;
 
-    function launcher(cmd, cwd, ondata, onexit) {
-        gloOnData = ondata || gloOnData;
-        gloOnExit = onexit || gloOnExit;
+    /**
+     * 运行命令
+     *
+     * @param {string} cmd 要运行的命令
+     * @param {string} cwd 命令运行的当前目录
+     * @param {Function} ondata 当运行输出数据到达时的处理行为
+     * @param {Function} onexit 命令运行结束的处理行为
+     */
+    function launch(cmd, cwd, ondata, onexit) {
+        if (launching) {
+            throw new Error('Command is launching!');
+        }
+
+        if (typeof ondata === 'function') {
+            gloOnData = ondata;
+        }
+
+        if (typeof onexit === 'function') {
+            gloOnExit = onexit;
+        }
+
         socket.emit('launch', {
             cmd: cmd,
             cwd: cwd
         });
+
+        launching = 1;
     }
 
+    /**
+     * 和后端WebSocket通信的对象
+     *
+     * @inner
+     * @type {Object}
+     */
     var socket;
-    launcher.init = function () {
+
+    /**
+     * 初始化命令运行器
+     */
+    launch.init = function () {
         socket = io.connect(location.origin);
 
         socket.on('cmd-stdout', function (data) {
-            gloOnData(data);
+            gloOnData && gloOnData(data);
         });
 
         socket.on('cmd-stderr', function (data) {
-            gloOnData(data);
+            gloOnData && gloOnData(data);
         });
 
         socket.on('cmd-exit', function () {
-            gloOnExit();
+            gloOnExit && gloOnExit();
+            launching = false;
         });
     };
 
-    return launcher;
+    return launch;
 });
